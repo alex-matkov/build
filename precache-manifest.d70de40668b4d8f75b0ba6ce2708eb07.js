b729a2948487a3d3970f6c13b5cc22804d48f304
alex-matkov <alex.matkov1@gmail.com>
2021-06-13 07:29:14 -0400

    fist

---
+++ precache-manifest.d70de40668b4d8f75b0ba6ce2708eb07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83fe908334ce20fd74ef60a09f0a53e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91dd58dd34eb8800903",
+    "url": "/static/js/2.8fb3e0e6.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.8fb3e0e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ff4a2c899f26d0b3ca",
+    "url": "/static/js/main.c32c011d.chunk.js"
+  },
+  {
+    "revision": "5e652e18ab901cbbf7b3",
+    "url": "/static/js/runtime-main.97411138.js"
+  },
+  {
+    "revision": "7849705fb4c962c43dcbbf0e11a8a5fe",
+    "url": "/static/media/about.7849705f.jpg"
+  },
+  {
+    "revision": "9c3a64e2b07b60f4f07f50bb804403b9",
+    "url": "/static/media/noImage.9c3a64e2.png"
+  }
+]);
